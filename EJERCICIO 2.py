5b585590d24e49c03c8a5c412bbe1da8562973b0
ybermudcas <ybermudcas@educacion.navarra.es>
2023-11-10 18:05:12 +0100

    EJERCICIO 2 DICC

--- EJERCICIO 2.py
+++ EJERCICIO 2.py
@@ -4,4 +4,4 @@ dirección = input("¿Cuál es tú dirección?")
 móvil = input("¿Cuál es tú número de móvil?")
 diccionario = {"Nombre": nombre, "Edad": edad, "Dirección": dirección, "Móvil": móvil}
 
-print("Nombre", "tiene", edad, "años,", "vive en", dirección, "y su número de móvil es", móvil)
+print(diccionario["Nombre"], "tiene", diccionario["Edad"], "años,", "vive en", diccionario["Dirección"], "y su número de móvil es", diccionario["Móvil"])
